e2eb56ed4f0c1c42c1f8442a1f08522427959640
noname0301 <vutranankhanh@gmail.com>
2025-12-30 10:24:41 +0700

    update colorfulness metric

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,3 +1,3 @@
-from .colorfulness import colorfulness, calculate_colorfulness
+from .colorfulness import calculate_colorfulness
 from .fid import calculate_fid
 from .psnr_ssim import calculate_psnr, calculate_ssim, calculate_ms_ssim
--- metrics/colorfulness.py
+++ metrics/colorfulness.py
@@ -1,43 +1,18 @@
-import numpy as np
-
-def colorfulness(image):
-    """
-    Compute the "colorfulness" metric of an RGB image.
-    
-    Parameters
-    ----------
-    image : np.ndarray
-        Input image, shape (3, H, W), values in [0,255] or [0,1].
-        If values are [0,1], function will scale internally to [0,255].
-    
-    Returns
-    -------
-    float
-        Colorfulness score (higher means more colorful)
-    """
-    if image.max() <= 1.0:
-        image = image * 255.0
-
-    R = image[0]
-    G = image[1]
-    B = image[2]
-
-    # rg = R - G
-    rg = R - G
-    # yb = 0.5*(R + G) - B
-    yb = 0.5 * (R + G) - B
-
-    # Compute the standard deviation and mean of rg and yb
-    std_rg = np.std(rg)
-    std_yb = np.std(yb)
-    mean_rg = np.mean(rg)
-    mean_yb = np.mean(yb)
-
-    # Combine the mean and standard deviation
-    colorfulness_score = np.sqrt(std_rg**2 + std_yb**2) + 0.3 * np.sqrt(mean_rg**2 + mean_yb**2)
-    return colorfulness_score
-
+import torch
 
 def calculate_colorfulness(images):
-    colorfulness_scores = [colorfulness(image) for image in images]
-    return np.mean(colorfulness_scores)
+    # images: (B, 3, H, W)
+    R, G, B = images[:,0], images[:,1], images[:,2]
+    rg = torch.abs(R - G)
+    yb = torch.abs(0.5 * (R+G) - B)
+    
+    rbMean = torch.mean(rg, dim=(1,2))
+    rbStd = torch.std(rg, dim=(1,2))
+    ybMean = torch.mean(yb, dim=(1,2))
+    ybStd = torch.std(yb, dim=(1,2))
+    
+    stdRoot = torch.sqrt(rbStd**2 + ybStd**2)
+    meanRoot = torch.sqrt(rbMean**2 + ybMean**2)
+    
+    colorfulness = stdRoot + 0.3 * meanRoot
+    return colorfulness.mean().item()
